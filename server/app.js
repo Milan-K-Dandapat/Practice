0a7f8360758ac5f8b32606a5fc3dce70daa36aca
Milan Kumar Dandapat <milankumar07@outlook.com>
2025-11-21 20:43:00 +0530

    message

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+//middelware
+
+const middleware = (req,res, next) => {
+    console.log(`hello my middleware`);
+    next(); 
+}
+
+app.get('/', (req, res) => {
+    res.send(`Hello World from the server`);
+});
+
+app.get('/about', middleware, (req, res) => {
+    res.send(`Hello about World from the server`);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(`Hello Contact World from the server`);
+});
+
+app.get('/signin', (req,res) => {
+    res.send(`Hello Login World from the server`);
+});
+
+app.get('/signup', (req,res) => {
+    res.send(`Hello Register World from the server`);
+});
+
+console.log('Milan kumar Dandapat')
+
+app.listen(3000, () => {
+    console.log(`Server is running at port no 3000`);
+})
